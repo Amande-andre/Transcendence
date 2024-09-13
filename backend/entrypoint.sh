7f4047b9a6be8b7c2bce5e14aa390067bed1f837
Franck Gojard <Franck.gojard@gmail.com>
2024-09-13 11:45:17 +0200

    back et front lier proleme resolu

---
+++ backend/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Appliquer les migrations
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+sleep 1
+
+# Supprimer les migrations existantes
+python manage.py flush --no-input
+# Créer les migrations Django
+echo "Making migrations..."
+python manage.py makemigrations
+
+# Exécuter les migrations Django
+echo "Running migrations..."
+python manage.py migrate
+
+# Démarrer Gunicorn
+echo "Starting Gunicorn..."
+gunicorn --bind 0.0.0.0:8000 --workers 3 transcendence.wsgi:application
