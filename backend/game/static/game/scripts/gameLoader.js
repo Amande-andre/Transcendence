16c4ab1de385905004ab3aae6d617151a86c4e42
Franck Gojard <Franck.gojard@gmail.com>
2024-11-05 13:13:57 +0100

    F: titre h1h2 du home querryselectorall pour les hide et d'autre petite modif css

--- backend/game/static/game/scripts/gameLoader.js
+++ backend/game/static/game/scripts/gameLoader.js
@@ -1,13 +1,13 @@
 let ctx;
 
 document.addEventListener('htmx:afterSwap', function(evt) {
-	if (evt.detail.target.id === 'nav') {
-		console.log('nav');
-		let elementToHide = document.getElementById('title');
-		if (elementToHide) {
-			elementToHide.style.display = 'none';
-    	}
-	}
+    if (evt.target.id === 'nav') {
+        console.log('nav clicked');
+        let elementsToHide = document.querySelectorAll('.title');
+        elementsToHide.forEach(function(element) {
+            element.style.display = 'none';
+        });
+    }
     if (evt.detail.target.id === 'gameOption') {
         let gameCanvas = document.getElementById('pongCanvas');
         if (gameCanvas) {
