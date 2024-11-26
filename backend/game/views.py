6679d9595f1d3cb324d3b30fef305664d0c9c74f
anmande <andremandevie@gmail.com>
2024-11-26 16:35:02 +0100

    manque game pour afficher le btn continue peut etre dans les games canvas...good luck

--- backend/game/views.py
+++ backend/game/views.py
@@ -7,11 +7,16 @@ import json
 def pongCanvas(request):
     # Récupérer les données des joueurs
 	players_raw = request.GET.get('players')
-	print("pong Players received:", players_raw)
-	return render(request, 'partials/pongCanvas.html', {'players': players_raw})
+	# print("pong Players received:", players_raw)	
+	# print("request:", request.GET.game)
+	game = request.GET.get('game')
+	return render(request, 'partials/pongCanvas.html', {'players': players_raw, 'game': game})
 
 def breakoutCanvas(request):
-	return render(request, 'partials/breakoutCanvas.html')
+	# Récupérer les données des joueurs
+	players_raw = request.GET.get('players')
+	game = request.GET.get('game')
+	return render(request, 'partials/breakoutCanvas.html', {'players': players_raw, 'game': game})
 
 def gameChoice(request):
 	return render(request, 'partials/gameChoice.html')
@@ -24,17 +29,17 @@ def optionsBreakout(request):
 
 def bracket(request):
     # Récupérer les données des joueursplay
-	print("bracket request.GET:", request.GET)
+	
 	players_raw = json.loads(request.GET.get('players'))
-	print("bracket Players received:", players_raw)
-	print("len players_raw:", len(players_raw))
 	list_bool = [False, False]
 	players2 = []
 	players3 = []
 	
+	print("game the big Game", request.GET.get('game'))
+	game = request.GET.get('game')
 	for players in players_raw:
 		if players['win'] >= 1:
 			players2.append(players)
 		if players['win'] >= 2:
 			players3.append(players)
-	return render(request, 'partials/bracket.html', {"players": players_raw, "players2": players2, "players3": players3})
+	return render(request, 'partials/bracket.html', {"players": players_raw, "players2": players2, "players3": players3, "game": game})
